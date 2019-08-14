63aba96a3a076c9a34bc5fd0bc27a5c31d3ece5a
anami823 <anami823@gmail.com>
2019-08-14 13:03:48 -0700

    Create movie model

--- models/movie.js
+++ models/movie.js
@@ -4,5 +4,10 @@ var movieSchema = new mongoose.Schema({
     title: String,
     releaseYear: Number,
     mpaaRating: String,
-    cast: [String]
+    cast: [String],
+    nowShowing: Boolean
 })
+
+// Compile the schema into a model and export it
+//mongoose.model('Name', schema)
+module.exports = mongoose.model('Movie', movieSchema)
